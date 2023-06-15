2ae165482d616a9327dd96a699c9dd7e7d793a13
Austin <harrenaustint@gmail.com>
2023-06-15 15:42:04 -0400

    push

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static('public'))
 const uri = process.env.ATLAS_URI.toString;
 
 mongoose.connect(uri, {useNewUrlParser: true,})
