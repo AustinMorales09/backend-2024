788b26c1819f485dcc32bae649454efad27a370b
Austin <harrenaustint@gmail.com>
2023-06-15 12:52:03 -0400

    pushing backend with production

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(uri, { useNewUrlParser: true })
 const connection = mongoose.connection;
 
 connection.once("open", () => {
