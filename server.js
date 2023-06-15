ff8acd23943bed2daf1ceb9b2b5ad250ec024dd5
Austin <harrenaustint@gmail.com>
2023-06-15 12:54:02 -0400

    pushing backend with production

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+// connection string
 const uri = process.env.ATLAS_URI;
 
 mongoose.connect(uri, { useNewUrlParser: true })
