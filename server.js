1859eb449ebacc8a70b7cfa92e9fe669b31a0b1f
Austin <harrenaustint@gmail.com>
2023-06-15 16:23:34 -0400

    push

--- server.js
+++ server.js
@@ -1,34 +1,40 @@
 const express = require('express');
-const app = express();
 const cors = require('cors')
 const mongoose = require('mongoose');
 require('dotenv').config();
+
+const app = express();
 const PORT = process.env.PORT || 5000;
+
 // Middleware
 app.use(cors());
 app.use(express.json());
-// Routes
+
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, })
 
+mongoose.connect(uri, {useNewUrlParser: true,})
 const connection = mongoose.connection;
+
 connection.once('open', () =>{
-    console.log("MongoDB database connection established succesfully");
+  console.log("MongoDB database connection established succesfully");
 })
+// Routes
+
+   const attractionsRouter = require('./routes/attractions')
+   app.use('/attractions', attractionsRouter) 
 
 
-const attractionsRouter = require('./routes/attractions')
-app.use('/attractions', attractionsRouter)
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-// app.get('/', (req, res) => res.render('home'));
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     next();
+//   });
+
+// Connect to MongoDB
+// comment
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-// Connect to MongoDB
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'))
 }
